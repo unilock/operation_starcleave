aae1e84987598a8ea6dd0086face2be097d50bb9
unilock <unilock@fennet.rentals>
2024-11-04 16:44:51 -0500

    port to 1.21
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/client/java/phanastrae/operation_starcleave/mixin/client/GameRendererMixin.java
+++ src/client/java/phanastrae/operation_starcleave/mixin/client/GameRendererMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import phanastrae.operation_starcleave.OperationStarcleaveClient;
 import phanastrae.operation_starcleave.render.ScreenShakeManager;
 import phanastrae.operation_starcleave.render.shader.FirmamentPostShader;
 
@@ -18,11 +19,10 @@ public class GameRendererMixin {
         ScreenShakeManager.getInstance().updateScreenMatrices(matrices, tickCounter.getTickDelta(false));
     }
 
-    // TODO
-//    @Inject(method = "updateTargetedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.BEFORE))
-//    private void operation_starcleave$updateFirmamentTarget(float tickDelta, CallbackInfo ci) {
-//        OperationStarcleaveClient.FirmamentOutlineRenderer.updateHitTile(tickDelta);
-//    }
+    @Inject(method = "updateCrosshairTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiler/Profiler;pop()V", shift = At.Shift.BEFORE))
+    private void operation_starcleave$updateFirmamentTarget(float tickDelta, CallbackInfo ci) {
+        OperationStarcleaveClient.FirmamentOutlineRenderer.updateHitTile(tickDelta);
+    }
 
     @Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;render(Lnet/minecraft/client/render/RenderTickCounter;ZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lorg/joml/Matrix4f;Lorg/joml/Matrix4f;)V", shift = At.Shift.AFTER))
     private void operation_starcleave$applyPostShaders(RenderTickCounter tickCounter, CallbackInfo ci) {
